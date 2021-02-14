a0cd7a2d38430834d0a97685fed94bf899f84771
Guilhermo Masid <guilhermomasid@gmail.com>
2021-02-14 16:51:33 -0300

    queries: user and getUserByEmail, mutation: createUser

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,64 @@
 import { ApolloServer, gql } from 'apollo-server'
 
-// Toda request é do tipo POST
-// Toda requst bate no MESMO endpoint ('/graphql')
-
-// Query -> Obter Informações (GET)
-// Mutation -> Manipular dados
-// Scalar Types -> String, Int, Boolean, Float e ID
+interface User {
+  _id: string
+  name: string
+  email: string
+  active: boolean
+}
 
 const typeDefs = gql`
+  type User {
+    _id: ID!
+    name: String!
+    email: String!
+    active: Boolean!
+  }
+
+  type Post {
+    _id: ID!
+    title: String!
+    content: String!
+    author: User!
+  }
+
   type Query {
     hello: String
+    users: [User!]!
+    getUserByEmail(email: String!): User!
+  }
+
+  type Mutation {
+    createUser(name: String!, email: String!): User!
   }
 `
 
+const users = [
+  { _id: String(Math.random()), name: 'Guilhermo 1', email: 'guilhermomasid@gmail.com', active: true },
+  { _id: String(Math.random()), name: 'Guilhermo 2', email: 'guilhermomasid@daktus.co', active: false },
+]
+
 const resolvers = {
   Query: {
-    hello: () => 'Hello World'
+    hello: () => 'Hello World',
+    users: () => users,
+    getUserByEmail: (_: unknown, args: User) => {
+      return users.find(user => user.email === args.email)
+    }
+  },
+  Mutation: {
+    createUser: (_: unknown, args: User) => {
+      const newUser = {
+        _id: String(Math.random()),
+        name: args.name,
+        email: args.email,
+        active: true
+      }
+
+      users.push(newUser)
+
+      return newUser
+    } 
   }
 }
 
