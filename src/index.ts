9a6f9502e71d950c0e474b10e05ab82c04519dcd
Guilhermo Masid <guilhermomasid@gmail.com>
2021-02-14 14:51:20 -0300

    creating index.ts

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+import { ApolloServer, gql } from 'apollo-server'
+
+const typeDefs = gql``
+const resolvers = {}
+
+const server = new ApolloServer({ typeDefs, resolvers })
+
+server.listen().then(({ url }) => console.log(`🦄 Server started at ${url}`))
